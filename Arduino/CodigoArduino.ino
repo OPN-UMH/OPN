07619606d3d9e83b0c376833a8040f949d3bda4b
OPN-UMH <enrike.ortiz@hotmail.com>
2014-04-01 19:28:19 +0200

    Código Arduino para control remoto de servomotor

---
+++ Arduino/CodigoArduino.ino
@@ -0,0 +1,86 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <Servo.h>
+
+
+// Introduce la dirección MAC y la dirección IP para la tarjeta Arduino:
+
+byte mac[] = { 0x38, 0xC8, 0x5C, 0x7F, 0xCF, 0x88 }; 
+byte ip[]={192,168,1,25 };
+
+byte server []= { 192,168,1,80}; // dirección IP del servidor
+int delayTime1 = 15;
+
+Servo miservo;
+int pos=0;
+int angulo=90; // Ángulo que queremos que gire nuestro servo
+char c;
+char aux=0;
+
+EthernetClient client;
+
+void setup()
+{
+  miservo.attach(9);// liga el servo conectado en el pin 9 al objeto servo  
+}
+
+void loop(){
+  connectToPC(); // Conectamos con el servidor
+
+  while (client.available()) {
+
+    c = client.read(); //Leemos la señal que proviene del servidor
+    Serial.println(c);
+     if(c==aux);
+     else{
+         if(c=='a'){ //Abre
+            moveForward();
+         }
+         else if(c=='c'){ //Cierra
+            moveBackward();
+         }
+    }
+}
+  
+
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("Disconnecting");
+    client.stop();
+    delay(6000);
+  }
+  //delay(3000);
+       aux=c; //Nos quedamos con el valor de la última acción para no repetirla
+}
+
+
+void connectToPC(){
+  Serial.begin(9600);
+  Ethernet.begin(mac, ip);
+  
+  delay(1000);
+
+  Serial.println("Connecting...");
+
+  if (client.connect(server, 5000)) {
+    Serial.println("Connected");
+    client.println("GET /search?q=arduino HTTP/1.0"); //NECESARIO PARA ESTABLECER LA CONEXIÓN
+  } 
+  else {
+    Serial.println("Connection failed");
+  }
+}
+  
+void moveForward() {
+  for(pos=0;pos<angulo;pos+=1){ 
+        miservo.write(pos); //Vamos incrementando el ángulo     
+        delay(delayTime1); //Controlamos la velocidad a partir del delay
+  }
+}
+
+void moveBackward() {
+  for(pos=angulo;pos>=1;pos-=1){
+        miservo.write(pos);
+        delay(delayTime1);    
+  }
+}
